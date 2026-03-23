6daafcace824ce306afdd0079be2ce8643e18100
Noah <ntallsbrook@tuta.com>
2026-03-22 22:26:17 -0400

    lots of stuff done; neck servo and tof multicore works

--- src/Controls.h
+++ src/Controls.h
@@ -1,11 +1,18 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <Adafruit_VL53L0X.h>
+#include <Wire.h>
 
-#define NECK_PIN P34
-#define JAW_PIN P35
+#define SDA P36
+#define SCL P37
+// #define ADDR 0x30
+
+#define NECK_PIN P4
+#define JAW_PIN P5
 
 #define NECK_MIN_ANGLE 0
 #define NECK_MAX_ANGLE 180
+#define NECK_CENTER_ANGLE 90
 
 #define JAW_MIN_ANGLE 0
 #define JAW_MAX_ANGLE 90
@@ -18,33 +25,90 @@
 
 Servo neck;
 Servo jaw;
+int currentNeckAngle;
+int stoppedNeckAngle;
+
+// TwoWire bus = TwoWire(1);
+// Adafruit_VL53L0X tof = Adafruit_VL53L0X();
+// VL53L0X_RangingMeasurementData_t measure;
+
+void stopHead();
+void centerHead();
+void continueMotion();
 
 void initControls()
 {
     neck.attach(NECK_PIN);
     jaw.attach(JAW_PIN);
+    // bus.begin(SDA, SCL);
+    // tof.begin(ADDR, &bus);
 
-    neck.write(NECK_MAX_ANGLE / 2);
+    // neck.write(NECK_CENTER_ANGLE);
+
+    currentNeckAngle = NECK_CENTER_ANGLE;
+    centerHead();
+
     jaw.write(JAW_MIN_ANGLE);
 }
 
-void turnHeadRight()
+void centerHead()
+{
+    if(currentNeckAngle < NECK_CENTER_ANGLE)
+    {
+        for(currentNeckAngle; currentNeckAngle < NECK_CENTER_ANGLE; currentNeckAngle++)
+        {
+            neck.write(currentNeckAngle);
+        }
+    }
+
+    else if(currentNeckAngle > NECK_CENTER_ANGLE)
+    {
+        for(currentNeckAngle; currentNeckAngle > NECK_CENTER_ANGLE; currentNeckAngle--)
+        {
+            neck.write(currentNeckAngle);
+        }
+    }
+
+    else 
+    {
+        currentNeckAngle = NECK_CENTER_ANGLE;
+    }
+}
+
+/*
+void turnHeadRight(bool detected)
 {
     for(int i = NECK_MIN_ANGLE; i < NECK_MAX_ANGLE; i++)
     {
         neck.write(i);
+        currentNeckAngle = i;
+        tof.rangingTest(&measure, false);
         delay(NECK_TURN_DELAY);
+
+        if(detected)
+        {
+            stopHead(); 
+        }
     }
 }
+*/
 
-void turnHeadLeft()
+/*
+void turnHeadLeft(bool detected)
 {
     for(int i = NECK_MAX_ANGLE; i > NECK_MIN_ANGLE; i--)
     {
         neck.write(i);
+        currentNeckAngle = i;
         delay(NECK_TURN_DELAY);
+
+        if(detected)
+        {
+            stopHead();
+        }
     }
 }
+*/
 
 void operateJaw()
 {
@@ -61,4 +125,23 @@ void operateJaw()
     jaw.write(i);
     delay(JAW_TURN_DELAY);
   }
+}
+
+void stopHead()
+{
+    neck.write(currentNeckAngle);
+    neck.detach();
+
+    stoppedNeckAngle = currentNeckAngle;
+
+    // operateJaw();
+    delay(2000);
+
+    continueMotion();
+}
+
+void continueMotion()
+{
+    neck.attach(NECK_PIN);
+    // neck.write(stoppedNeckAngle);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,32 @@
 
 #define SDA	P36
 #define SCL	P37
+#define SHUTDOWN P34
+// #define INT P22
+#define ADDR 0x30
+#define DETECT_RANGE 500
 
 Adafruit_VL53L0X tof = Adafruit_VL53L0X();
-TwoWire bus = TwoWire(0);
+TwoWire bus = TwoWire(1);
+// bool detected;
+// VL53L0X_RangingMeasurementData_t measure;
+// int tofReading;
+
+bool checkInRange(int, int, int);
+void stopIdle(bool);
+void turnHeadRight(int);
+void turnHeadLeft(int);
+void readTof(int);
+
+void idle(void *);
+void reading(void *);
+
+TaskHandle_t Idle;
+TaskHandle_t Read;
 
 void setup()
 {
+	// delay(1000);
 	Serial.begin(115200);
 
 	while (!Serial)
@@ -19,40 +39,127 @@ void setup()
 		delay(1);
 	}
 
-	bus.begin(SDA, SCL);
+	if (!bus.begin(SDA, SCL))
+	{
+		Serial.println("Failed to initialize i2c bus!");
+	}
 
-	if (!tof.begin(0x29, &bus))
+	Serial.println("Initialized i2c bus!");
+
+	digitalWrite(SHUTDOWN, LOW);
+
+	if (!tof.begin(ADDR, true, &bus))
 	{
 		Serial.println("Initialization of VL53L0X failed!");
 		while(true);
 	}
 
+	digitalWrite(SHUTDOWN, HIGH);
 	Serial.println("Successfully initialized VL53L0X!");
+
+	// digitalWrite(INT, HIGH);
+	initControls();
+
+	xTaskCreatePinnedToCore(
+		idle,
+		"Idle",
+		10000,
+		NULL,
+		1,
+		&Idle,
+		0
+	);
+
+	xTaskCreatePinnedToCore(
+		reading,
+		"Read",
+		200000,
+		NULL,
+		1,
+		&Read,
+		1
+	);
 }
 
 void loop()
 {
 
-	VL53L0X_RangingMeasurementData_t measure;
-	tof.rangingTest(&measure, false);
+}
 
-	if (measure.RangeStatus != 4)
+/*
+void readTof(int idealRange)
+{
+	tofReading = tof.rangingTest(&measure, false);
+	if(checkInRange(measure.RangeStatus, measure.RangeMilliMeter, idealRange))
 	{
-		Serial.println("Distance (mm): " + (String)measure.RangeMilliMeter);
+		Serial.println("Blocked!");
+		stopHead();
 	}
 
-	else
+	else 
 	{
-		Serial.println("Out of range.");
+		Serial.println("Not blocked.");
 	}
+}
+*/
 
-	turnHeadRight();
-	delay(NECK_STOP_DELAY);
+bool checkInRange(int status, int distance, int range)
+{
+	return status != 4 && distance <= range;
+}
 
-	operateJaw();
+void turnHeadRight()
+{
+    for(int i = NECK_MIN_ANGLE; i < NECK_MAX_ANGLE; i++)
+    {
+		// readTof(idealRange);
+        neck.write(i);
+        currentNeckAngle = i;
+        // tof.rangingTest(&measure, false);
+    	vTaskDelay(NECK_TURN_DELAY / portTICK_PERIOD_MS);
+    }
+}
 
-	turnHeadLeft();
-	delay(NECK_STOP_DELAY);
+void turnHeadLeft()
+{
+    for(int i = NECK_MAX_ANGLE; i > NECK_MIN_ANGLE; i--)
+    {
+		// readTof(idealRange);
+        neck.write(i);
+        currentNeckAngle = i;
+		// tof.rangingTest(&measure, false);
+        vTaskDelay(NECK_TURN_DELAY / portTICK_PERIOD_MS);
+    }
+}
 
-	operateJaw();
+void idle(void *parameters)
+{
+	for(;;)
+	{
+		centerHead();
+
+		turnHeadLeft();
+		vTaskDelay(NECK_STOP_DELAY / portTICK_PERIOD_MS);
+
+		turnHeadRight();
+		vTaskDelay(NECK_STOP_DELAY / portTICK_PERIOD_MS);
+	}
+}
+
+void reading(void *parameters)
+{
+	for(;;)
+	{
+		VL53L0X_RangingMeasurementData_t measure;
+		tof.rangingTest(&measure, false);
+		Serial.println(measure.RangeMilliMeter);
+		if(checkInRange(measure.RangeStatus, measure.RangeMilliMeter, DETECT_RANGE))
+		{
+			Serial.println("Blocked!");
+			Serial.println(measure.RangeMilliMeter);
+			stopHead();
+		}
+
+		// vTaskDelay(50 / portTICK_PERIOD_MS);
+	}
 }
